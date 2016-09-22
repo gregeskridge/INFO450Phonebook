599b94e24ecef6bb50903290afd718b47b77f4c0
Gregory Eskridge <gregeskridge@gmail.com>
2016-09-22 16:45:57 -0400

    Version 1

--- INFO450Phonebook/INFO450Phonebook.cpp
+++ INFO450Phonebook/INFO450Phonebook.cpp
@@ -2,10 +2,45 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+const int MAXENTRY = 10;
+const int MAXPARTS = 2;
+const int MAXLENGTH = 50;
+
+void AddEntry(char phoneBook[][][MAXLENGTH]);
+void DisplayEntry(char phoneBook[][][MAXLENGTH]);
 
 
 int main()
 {
+	char phoneList[MAXENTRY][MAXPARTS][MAXLENGTH];
+	int i;
+	char answer = 'y';
+
+	for (i = 0; i < MAXENTRY && (answer == 'y' || answer == 'Y'); i++)
+	{
+		AddEntry(phoneList);
+		cout << "Would you like to enter another name?" << endl;
+		cin >> answer;
+		cin.clear();
+		cin.ignore();
+	}
+	
+	cout << "Here i = " << i << endl;
+	DisplayEntry(phoneList, i);
+
     return 0;
 }
 
+void AddEntry(char phoneBook[][][MAXLENGTH])
+{
+
+}
+
+void DisplayEntry(char phoneBook[][][MAXLENGTH])
+{
+
+}
