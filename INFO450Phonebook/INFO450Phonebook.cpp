d092cd8829b8b865852f4e513aa3de009f6fbe86
Gregory Eskridge <gregeskridge@gmail.com>
2016-09-23 16:49:23 -0400

    Final Version

--- INFO450Phonebook/INFO450Phonebook.cpp
+++ INFO450Phonebook/INFO450Phonebook.cpp
@@ -1,9 +1,10 @@
-// INFO450Phonebook.cpp : Defines the entry point for the console application.
+// INFO450Phonebook.cpp : This allows you to create a phonebook, check for duplicate entries, and display said phonebook.
 //
 
 #include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 using namespace std;
 
 const int MAXENTRY = 10;
@@ -18,20 +19,44 @@ int main()
 {
 	char phoneList[MAXENTRY][MAXPARTS][MAXLENGTH];
 	int i;
-	char answer = 'y';
+	char answer = 'a';
 
-	for (i = 0; i < MAXENTRY && (answer == 'y' || answer == 'Y'); i++)
+	for (i = 0; i < MAXENTRY && (answer == 'D' || answer == 'a' || answer == 'D' || answer == 'd'); i++)
 	{
-		AddEntry(phoneList);
-		cout << "Would you like to enter another name?" << endl;
+		cout << "--------------------------" << endl;
+		cout << "What would you like to do?" << endl;
+		cout << "--------------------------" << endl;
+		cout << "\t[A]dd a new entry -- enter A" << endl;
+		cout << "\t[D]isplay all entries -- enter D" << endl;
+		cout << "\t[Q]uit this application -- enter Q" << endl;
 		cin >> answer;
-		cin.clear();
-		cin.ignore();
+		if (answer != 'A'	&& answer != 'a' && answer != 'D' && answer != 'd' && answer != 'Q' && answer != 'q')
+		{
+			cin.clear();
+			cin.ignore();
+			cout << "Your entry is invalid.  Please enter a valid character." << endl;
+			cin >> answer;
+			i--;
+		}
+		else if(answer == 'A' || answer == 'a')
+		{
+			cin.clear();
+			cin.ignore();
+			AddEntry(phoneList);
+		}
+		else if (answer == 'D' || answer == 'd')
+		{
+			cin.clear();
+			cin.ignore();
+			DisplayEntry(phoneList, i);
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore();
+		}		
 	}
 	
-	cout << "Here i = " << i << endl;
-	DisplayEntry(phoneList, i);
-
     return 0;
 }
 
@@ -44,8 +69,24 @@ void AddEntry(char phoneBook[][MAXPARTS][MAXLENGTH])
 	}
 	else
 	{
+		int a;
+		
+		cout << "Entry Number " << i + 1 << ":" << endl;
 		cout << "Give me a name.  Just don't give me a duplicate name: " << endl;
 		gets_s(phoneBook[i][0]);
+
+		for (a = 0; a < i; a++)
+		{ 
+			if (strcmp(phoneBook[i][0], phoneBook[a][0]) == 0)
+			{
+				cout << "You committed a nono.  Please enter a new name." << endl;
+				gets_s(phoneBook[i][0]);
+			}
+			else
+			{
+
+			}
+		}
 		cout << "Give me the phone number for that person: " << endl;
 		gets_s(phoneBook[i][1]);
 		i++;
@@ -57,6 +98,6 @@ void DisplayEntry(char phoneBook[][MAXPARTS][MAXLENGTH], int iValue)
 	int i;
 	for (i = 0; i < iValue; i++)
 	{
-		printf("%s\t%s\n", phoneBook[i][0], phoneBook[i][1]);
+		printf("Entry Number %i:\t%s\t%s\n", i+1, phoneBook[i][0], phoneBook[i][1]);
 	}
 }
